1392d48c9491dd62f892f8370d7dda1af13f17f5
suhuakai1994 <49890169+suhuakai1994@users.noreply.github.com>
2019-04-23 10:22:38 +0800

    Create Constant.java

---
+++ common/Constant.java
@@ -0,0 +1,16 @@
+package com.huawei.oss.at.corba.common;
+
+public final class Constant {
+    private Constant() {
+    }
+
+    public static final String STR_COLON = ":";
+
+    public static final String STR_CONCAT = "_^_";
+
+    public static final String TOKEN_NAMING_PATH = "IMAP/Services/SessionManager";
+
+    public static final String TOKEN_IDL_PATH = "IDL:iMAPsm/ProductSessionManager/getTokenByBspsession:1.0";
+
+    public static final String TOKEN_OPERATION = "getTokenByBspsession";
+}
